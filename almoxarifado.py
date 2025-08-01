a9ff91d6b5a7319fd68df4056dcbe4e44f59a1a7
Natan Mauricio Santos <natanmauriciosantos@hotmail.com>
2025-08-01 03:27:42 +0000

    ALMOXARIFADO - INGREDIENTES

---
+++ almoxarifado.py
@@ -0,0 +1,67 @@
+produtos = {
+    "trigo" : {
+        "preco" : 4.5,
+        "quantidade" : 100,
+        "minimo" : 50
+    },
+    
+    "acucar_refinado" : {
+        "preco" : 4.2,
+        "quantidade" : 100,
+        "minimo" : 50
+    },
+    
+    "acucar_mascavo" : {
+        "preco" : 6.0,
+        "quantidade" : 100,
+        "minimo" : 50
+    },
+    
+    "ovo" : {
+        "preco" : 0.8,
+        "quantidade" : 100,
+        "minimo" : 50
+    },
+    
+    "manteiga" : {
+        "preco" : 28.0,
+        "quantidade" : 100,
+        "minimo" : 50
+    },
+    
+    "chocolate_leite" : {
+        "preco" : 35.0,
+        "quantidade" : 100,
+        "minimo" : 50
+    },
+    
+    "chocolate_amargo" : {
+        "preco" : 35.0,
+        "quantidade" : 100,
+        "minimo" : 50
+    },
+    
+    "fermento_biologico" : {
+        "preco" : 50.0,
+        "quantidade" : 100,
+        "minimo" : 50
+    },
+    
+    "bicarbonato_sodio" : {
+        "preco" : 18.0,
+        "quantidade" : 100,
+        "minimo" : 50
+    },
+    
+    "leite" : {
+        "preco" : 4.0,
+        "quantidade" : 100,
+        "minimo" : 50
+    },
+    
+    "oleo" : {
+        "preco" : 7.5,
+        "quantidade" : 100,
+        "minimo" : 50
+    }
+}
